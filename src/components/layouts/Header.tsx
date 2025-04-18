b31a0a82af16eb028b694f3c59cdb5cb834d5335
cibeys <132761045+cibeys@users.noreply.github.com>
2025-04-19 06:52:39 +0700

    Update Header.tsx
    change header logo
    
    Signed-off-by: cibeys <132761045+cibeys@users.noreply.github.com>

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
           {/* Logo dan menu desktop */}
           <div className="flex items-center">
             <div className="flex-shrink-0 flex items-center">
-              <span className="text-xl font-bold dark:text-white">Logo</span>
+              <span className="text-xl font-bold dark:text-white">vitreasta</span>
             </div>
             <div className="hidden md:ml-6 md:flex md:space-x-8">
               {navItems.map((item) => (
